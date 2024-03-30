abaf4b16d94af83744c04f5941c813a034adecca
Lewis Munene Irimu <98595640+irimiah@users.noreply.github.com>
2024-03-30 13:52:47 +0300

    js virtual tour

---
+++ tour.js
@@ -0,0 +1,86 @@
+const Data = [
+  {
+    data: {
+      peter: [
+        {
+          sceneId: "circle",
+          title: "Peter Kinyanjui",
+          hfov: 110,
+          yaw: -3,
+          pitch: 10,
+          panorama: "https://img.lovepik.com/photo/50091/9982.jpg_wh860.jpg",
+          hotSpots: [
+            {
+              pitch: -2.1,
+              yaw: 132.9,
+              type: "scene",
+              text: "Sting Room",
+              sceneId: "house",
+            },
+          ],
+        },
+        {
+          sceneId: "house",
+          title: "Spring House or Dairy",
+          hfov: 110,
+          yaw: 5,
+          pitch: 0,
+          panorama:
+            "https://st4.depositphotos.com/20782690/23870/i/450/depositphotos_238705674-stock-photo-panorama-360-view-modern-white.jpg",
+          autoRotateSpeed: 0.3,
+          hotSpots: [
+            {
+              pitch: -0.6,
+              yaw: 37.1,
+              type: "scene",
+              text: "Bed Room",
+              sceneId: "circle",
+              targetYaw: -23,
+              targetPitch: 2,
+            },
+          ],
+        },
+      ],
+      helm: [
+        {
+          sceneId: "circle",
+          title: "Peter Kinyanjui",
+          hfov: 110,
+          yaw: -3,
+          pitch: 10,
+          panorama: "https://img.lovepik.com/photo/50091/9982.jpg_wh860.jpg",
+          hotSpots: [
+            {
+              pitch: -2.1,
+              yaw: 132.9,
+              type: "scene",
+              text: "Sting Room",
+              sceneId: "house",
+            },
+          ],
+        },
+        {
+          sceneId: "house",
+          title: "Spring House or Dairy",
+          hfov: 110,
+          yaw: 5,
+          pitch: 0,
+          panorama:
+            "https://st4.depositphotos.com/20782690/23870/i/450/depositphotos_238705674-stock-photo-panorama-360-view-modern-white.jpg",
+          autoRotateSpeed: 0.3,
+          hotSpots: [
+            {
+              pitch: -0.6,
+              yaw: 37.1,
+              type: "scene",
+              text: "Bed Room",
+              sceneId: "circle",
+              targetYaw: -23,
+              targetPitch: 2,
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
